4f69cc619f7b58c87560469979abf61a4ab6eb6b
“xinsir” <2418074134@qq.com>
2022-06-01 08:45:00 +0800

    添加了角色成员页面

--- src/views/ManagementView.vue
+++ src/views/ManagementView.vue
@@ -31,10 +31,115 @@
         </div>
         <div class="tab">
           <el-tabs v-model="activeName" @tab-click="handleClick">
-            <el-tab-pane label="角色成员" name="first">
-              <span v-for="(item, index) in options" :key="index">
+            <el-tab-pane label="角色成员" name="first" class="role">
+              <div class="flex-between role-header">
+                <div>
+                  <span>小千金</span>
+                  <span class="el-icon-user"></span>
+                  <span>1</span>
+                </div>
+                <div class="font-color">
+                  <span class="el-icon-user"></span>
+                  <span>已离职成员</span>
+                  <span class="el-icon-user"></span>
+                  <span>邀请成员加入本组织</span>
+                </div>
+              </div>
+              <!--  -->
+              <div class="flex-between role-container">
+                <div class="align-center">
+                  <div>
+                    <el-select v-model="value" placeholder="请选择">
+                      <el-option
+                        v-for="item in optionsName"
+                        :key="item.value"
+                        :label="item.label"
+                        :value="item.value"
+                      >
+                      </el-option>
+                    </el-select>
+                  </div>
+                  <div>
+                    <el-button class="btn"
+                      ><img src="@/assets/icon-menus.png" alt=""
+                    /></el-button>
+                  </div>
+                </div>
+                <div>
+                  <el-button type="primary" class="btn">
+                    <span class="el-icon-plus"></span>
+                    <span>添加成员</span>
+                  </el-button>
+                  <el-button class="btn"> 批量导入/导出 </el-button>
+                  <el-button class="btn"> 变更部门 </el-button>
+                  <el-button class="btn"> 操作离职 </el-button>
+                </div>
+              </div>
+              <!--  -->
+              <div class="role-tab">
+                <el-table
+                  max-height="360"
+                  ref="multipleTable"
+                  :data="tableData"
+                  tooltip-effect="dark"
+                  style="width: 100%"
+                  @selection-change="handleSelectionChange"
+                >
+                  <el-table-column type="selection" width="80" align="center">
+                  </el-table-column>
+                  <el-table-column label="姓名" width="150" align="center">
+                    <template slot-scope="scope">{{ scope.row.date }}</template>
+                  </el-table-column>
+                  <el-table-column
+                    prop="status"
+                    label="账号状态"
+                    width="200"
+                    align="center"
+                  >
+                  </el-table-column>
+                  <el-table-column
+                    prop="phone"
+                    label="手机"
+                    align="center"
+                    width="300"
+                  >
+                  </el-table-column>
+                  <el-table-column
+                    prop="department"
+                    label="部门"
+                    align="center"
+                    width="200"
+                  >
+                  </el-table-column>
+                  <el-table-column align="center" width="100">
+                    <el-tag>
+                      <span class="el-icon-arrow-left"></span>
+                    </el-tag>
+                  </el-table-column>
+                </el-table>
+              </div>
+              <div class="role-footer flex-between">
+                <div>
+                  <el-switch
+                    v-model="value1"
+                    active-color="#13ce66"
+                    inactive-color="#ff4949"
+                  >
+                  </el-switch>
+                  仅展示部门的直属成员
+                </div>
+                <div>
+                  <el-pagination
+                    background
+                    layout="prev, pager, next"
+                    :total="1000"
+                  >
+                  </el-pagination>
+                </div>
+              </div>
+              <!-- <span v-for="(item, index) in options" :key="index">
                 {{ item.roleName }}
-              </span>
+              </span> -->
             </el-tab-pane>
             <el-tab-pane label="功能权限" name="second">
               <managements-view></managements-view>
@@ -57,9 +162,12 @@ export default {
   },
   data() {
     return {
-      activeName: "second",
+      activeName: "first",
       data: [],
       options: [],
+      optionsName: [],
+      value: "",
+      value1: true,
       groupData: [],
       defaultProps: {
         children: "children",
@@ -67,6 +175,51 @@ export default {
       },
       roleList: [],
       groupList: [],
+      tableData: [
+        {
+          date: "2016-05-03",
+          status: "王小虎",
+          phone: "上海市普陀区金沙江路 1518 弄",
+          department: "管理",
+        },
+        {
+          date: "2016-05-03",
+          status: "王小虎",
+          phone: "上海市普陀区金沙江路 1518 弄",
+          department: "管理",
+        },
+        {
+          date: "2016-05-03",
+          status: "王小虎",
+          phone: "上海市普陀区金沙江路 1518 弄",
+          department: "管理",
+        },
+        {
+          date: "2016-05-03",
+          status: "王小虎",
+          phone: "上海市普陀区金沙江路 1518 弄",
+          department: "管理",
+        },
+        {
+          date: "2016-05-03",
+          status: "王小虎",
+          phone: "上海市普陀区金沙江路 1518 弄",
+          department: "管理",
+        },
+        {
+          date: "2016-05-03",
+          status: "王小虎",
+          phone: "上海市普陀区金沙江路 1518 弄",
+          department: "管理",
+        },
+        {
+          date: "2016-05-03",
+          status: "王小虎",
+          phone: "上海市普陀区金沙江路 1518 弄",
+          department: "管理",
+        },
+      ],
+      multipleSelection: [],
     };
   },
   async created() {
@@ -76,6 +229,9 @@ export default {
     this.getMenuList();
   },
   methods: {
+    handleSelectionChange(val) {
+      this.multipleSelection = val;
+    },
     async getMenuList() {
       let [roleData, groupData] = await Promise.all([
         queryRoleListApi({ pagination: false }),
@@ -154,6 +310,28 @@ export default {
       & .tab {
         padding: 10px;
         text-indent: 10px;
+        & .font-color {
+          color: #87abff;
+        }
+        & .btn {
+          height: 40px;
+        }
+        & .role-container {
+          padding: 10px;
+          text-indent: 0;
+        }
+        & .role-tab {
+          & .el-table {
+            ::-webkit-scrollbar {
+              height: 0;
+              width: 0;
+            }
+          }
+        }
+        & .role {
+          display: grid;
+          grid-template-rows: 40px 60px auto 50px;
+        }
       }
     }
   }
